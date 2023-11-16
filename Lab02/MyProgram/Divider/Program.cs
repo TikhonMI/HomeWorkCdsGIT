47a6859c8e82c7d5e1b155e7d839f070975f4eab
tikhon mi <gvozdev.te@mekhanobr.com>
2023-11-16 17:29:24 +0300

    cds lab0, lab1(1,2,3,4)

---
+++ Lab02/MyProgram/Divider/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideIt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the first integer");
+                int i = Int32.Parse(Console.ReadLine()); //переводим вводимое значение в int через Int32.Parse
+                Console.WriteLine("Please enter the second integer");
+                int j = Convert.ToInt32(Console.ReadLine()); //переводим вводимое значение в int через Convert.ToInt32 (Мы так можем делать?)
+                int k = i / j;
+                Console.WriteLine($"The result of dividing {i} by {j} is {k}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An format exception was thrown: {0}",
+                e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e.Message);
+            }
+        }
+    }
+}
