456cbce4ab1f5dd700681a872443a7d58a6e5e30
tikhon mi <gvozdev.te@mekhanobr.com>
2023-11-17 11:14:58 +0300

    lab1(5)

---
+++ KRLab01/S_Triang/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace S_Triang
+{
+    class Triangle
+    {
+        double side = 0.0;
+        double area = 0.0;
+
+        public Triangle(double perimeter) 
+        {
+            side = perimeter / 3;
+            area = Math.Sqrt(3) / 4 * (side*side);
+        }
+
+        public void parameterOutput()
+        {
+            Console.WriteLine("Сторона   Площадь");
+            Console.WriteLine("{0:F2}     {1:F2}", side, area);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double per = double.Parse(Console.ReadLine());
+
+            Triangle Tr1 = new Triangle(per);
+            Tr1.parameterOutput();
+        }
+    }
+}
---
+++ KRLab01/S_Triang/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("S_Triang")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("S_Triang")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("870b53bb-41d7-4b60-a8e6-7c8960439032")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
