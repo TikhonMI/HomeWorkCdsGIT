4ec705f1e7f56e18ea95cd0533be2bf160e405b1
tikhon mi <gvozdev.te@mekhanobr.com>
2023-11-21 16:47:32 +0300

    labA

---
+++ LabA/Sqrt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sqrt
+{
+    internal class Program
+    {
+        static double MySqrt(double target)
+        {
+            double x = 1;
+            double oldx;
+            do
+            {
+                oldx = x;
+                x = (x + target / x) / 2;
+            }
+            while (oldx != x);
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            double target = 2023;
+
+            double x=MySqrt(target);
+            
+            Console.WriteLine(x);
+            Console.WriteLine(x * x);
+        }
+    }
+}
---
+++ LabA/SumOfTheNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое, положительное число: ");
+            string number = Console.ReadLine();
+            int total = 0;
+
+            foreach (char num in number)
+            {
+                total += (int)num - '0';
+            }
+            Console.WriteLine($"Сумма всех цифр числа = {total}");
+        }
+    }
+}
