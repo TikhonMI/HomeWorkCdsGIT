d363eb3738248d24ab375ca7d874d305a74a430c
tikhon mi <gvozdev.te@mekhanobr.com>
2023-11-22 17:48:12 +0300

    lab05

---
+++ Lab05/TestWork/AreaOfATriangle/Operation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AreaOfATriangle
+{
+    public class Operation
+    {
+        public static double Area (double a, double b, double c)
+        {
+            if (PresenceOfATriangle(a, b, c))
+            {
+                double pPerimeter = (a + b + c) / 2;
+                double s = Math.Sqrt(pPerimeter * (pPerimeter - a) * (pPerimeter - b) * (pPerimeter - c));
+                return s;
+            }
+            else 
+            {              
+                return 0; 
+            }
+        }
+        public static double Area (double a)
+        {
+            if (PresenceOfATriangle(a, a, a))
+            {
+                double s = ((a * a)* Math.Sqrt(3))/4;
+                return s;
+            }
+            else
+            {                
+                return 0;
+            }
+        }
+        private static bool PresenceOfATriangle(double a, double b, double c)
+        {
+            bool i;
+            i = (a + b + c) / 2 > a && (a + b + c) / 2 > b && (a + b + c) / 2 > c ? true : false;
+            if (i == false)
+            {
+                Console.WriteLine("Стороны не образуют треугольник!");
+            }
+            return i;
+        }
+    }
+}
---
+++ Lab05/TestWork/AreaOfATriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AreaOfATriangle
+{
+    internal class Program
+    {
+        static double InputSide()
+        {
+            Console.Write("\nВведите сторону треугольника: ");
+            double a = double.Parse(Console.ReadLine());
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите тип треугольника:");
+            int triangleType;
+            do
+            {                
+                Console.WriteLine("\n0-равносторонний \n1-неравносторонний");
+                var k = Console.ReadKey();          
+                triangleType = (k.KeyChar) - '0';
+            }
+            while (!(triangleType==0||triangleType==1));
+
+            double s;
+            if (triangleType == 0)
+            {
+                double a=InputSide();
+                s = Operation.Area(a);                           
+            }
+            else
+            {
+                double a = InputSide();
+                double b = InputSide();
+                double c = InputSide();
+                s = Operation.Area(a, b ,c);
+            }
+            Console.WriteLine($"Площадь треугольника = {s}") ;
+        }
+    }
+}
