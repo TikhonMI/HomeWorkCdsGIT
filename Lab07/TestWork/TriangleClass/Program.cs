3f6e70962f259522050aaf03b920ce72182bf3db
tikhon mi <gvozdev.te@mekhanobr.com>
2023-11-27 13:22:04 +0300

    Lab6(1-3)

---
+++ Lab07/TestWork/TriangleClass/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static Triangle NewTriangle()
+    {
+        Triangle create = new Triangle();
+        double sideA = InputSide('a');
+        double sideB = InputSide('b');
+        double sideC = InputSide('c');
+        create.RecordingSides(sideA, sideB, sideC);
+        return create;
+    }
+    static double InputSide(char i)
+    {        
+        Console.Write($"Введите сторону треугольника {i}: ");
+        double a = double.Parse(Console.ReadLine());
+        return a;
+    }
+    static void OutPresenceTriangle(Triangle met)
+    {        
+        if (met.PresenceOfATriangle())
+        {
+            Console.WriteLine("Стороны образуют треугольник!");
+        }
+        else
+        {
+            Console.WriteLine("Стороны не образуют треугольник!");
+        }
+    }
+    static void OutPerimetr(Triangle met)
+    {
+        Console.WriteLine($"Периметр треугольника = {met.Perimeter()}");
+    }
+    static void OutSTriangle(Triangle met)
+    {
+        Console.WriteLine($"Площадь треугольника = {met.STriangle()}");
+    }
+    static void OutSides(Triangle met)
+    {
+        var kor = met.OutputSides();
+        (double a, double b, double c) = kor;
+        Console.WriteLine($"Сторона a = {a}, Сторона b = {b}, Сторона c = {c}.");
+    }
+
+    static void Main(string[] args)
+    {
+        Triangle triangle1 = NewTriangle();
+        
+        OutPresenceTriangle(triangle1);
+        OutPerimetr(triangle1);
+        OutSTriangle(triangle1);
+        OutSides(triangle1);               
+    }
+     
+}
---
+++ Lab07/TestWork/TriangleClass/Triangle.cs
@@ -0,0 +1,41 @@
+﻿
+class Triangle
+{
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private double perABC;
+    private double sABC;
+
+    public void RecordingSides(double a, double b, double c)
+    {
+        sideA = a;
+        sideB = b;
+        sideC = c;
+
+        Perimeter();
+    }
+    public bool PresenceOfATriangle()
+    {        
+        bool i;
+        double Pper = perABC / 2;     
+        i = Pper > sideA && Pper > sideB && Pper > sideC ? true : false;
+        return i;
+    }
+    public double Perimeter()
+    {
+        perABC = sideA + sideB + sideC;
+        return perABC;
+    }
+    public double STriangle()
+    {        
+        double Pper = perABC / 2;
+        sABC = Math.Sqrt(Pper * (Pper - sideA) * (Pper - sideB) * (Pper - sideC));        
+        return sABC;
+    }
+    public (double a, double b, double c) OutputSides()
+    {
+        return (sideA, sideB, sideC);
+    }
+}
+
