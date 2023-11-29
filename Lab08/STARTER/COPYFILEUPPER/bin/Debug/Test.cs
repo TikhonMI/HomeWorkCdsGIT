14fbf20001420319a8f7745b75414ca679cd1ca7
tikhon mi <gvozdev.te@mekhanobr.com>
2023-11-29 09:31:17 +0300

    Lab07(1-5)

---
+++ Lab08/STARTER/COPYFILEUPPER/bin/Debug/Test.cs
@@ -0,0 +1,45 @@
+    USING SYSTEM;
+USING SYSTEM.IO;
+
+    /// <SUMMARY>
+    ///    CLASS TO CREATE AN UPPER CASE COPY OF A FILE
+    /// </SUMMARY>
+    PUBLIC CLASS COPYFILEUPPER
+    {
+		PUBLIC STATIC VOID MAIN()
+		{
+        STRING SFROM, STO;
+        STREAMREADER SRFROM;
+        STREAMWRITER SWTO;
+
+        CONSOLE.WRITE("COPY FROM:");
+        SFROM = CONSOLE.READLINE();
+
+        CONSOLE.WRITE("COPY TO:");
+        STO = CONSOLE.READLINE();
+
+        CONSOLE.WRITELINE("COPY FROM {0} TO {1}", SFROM, STO);
+        TRY
+        {
+            SRFROM = NEW STREAMREADER(SFROM);
+            SWTO = NEW STREAMWRITER(STO);
+            WHILE (SRFROM.PEEK() != -1)
+            {
+                STRING SBUFFER = SRFROM.READLINE();
+                SBUFFER = SBUFFER.TOUPPER();
+                SWTO.WRITELINE(SBUFFER);
+            }
+            SWTO.CLOSE();
+            SRFROM.CLOSE();
+        }
+        CATCH (FILENOTFOUNDEXCEPTION)
+        {
+            CONSOLE.WRITELINE("INPUT FILE NOT FOUND");
+        }
+        CATCH (EXCEPTION E)
+        {
+            CONSOLE.WRITELINE("UNEXPECTED EXCEPTION");
+            CONSOLE.WRITELINE(E.TOSTRING());
+        }
+    }       
+    }
---
+++ Lab08/STARTER/UTILS/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Test
+    {
+        static void Main()
+        {
+            string message;
+            
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+            
+            Utils.Reverse(ref message);
+            
+            Console.WriteLine(message);
+        }
+    }
+}
