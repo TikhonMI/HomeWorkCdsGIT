a08778d0378eb7c7cdcd422ffcdadc866fe1c48e
tikhon mi <gvozdev.te@mekhanobr.com>
2023-12-03 23:22:10 +0300

    Lab11

---
+++ Lab12/testWork/DistanceInch/Distance.cs
@@ -0,0 +1,40 @@
+﻿
+namespace DistanceInch
+{   
+    internal class Distance
+    {
+        private int foot;
+        private int inch;
+
+        public Distance (int foot, int inch)
+        {
+            this.foot = foot;
+            this.inch = inch;
+        }
+        private int AllInch()
+        {
+            return foot * 12 + inch;
+        }
+        public static Distance operator + (Distance d1, Distance d2)
+        {
+            int allInch = (d1.foot + d2.foot) * 12 + d1.inch + d2.inch;
+            int newFoot = allInch / 12;
+            int newInch = allInch % 12;
+            return new Distance (newFoot, newInch);
+        }
+        public static Distance operator -(Distance d1, Distance d2)
+        {            
+            int allInch = d1.AllInch() - d2.AllInch();
+            int newFoot = allInch / 12;
+            int newInch = allInch % 12;
+            return new Distance(newFoot, newInch);
+        }
+        public static bool operator == (Distance d1, Distance d2) => (_ = d1.AllInch() == d2.AllInch()? true: false);
+       
+        public static bool operator !=(Distance d1, Distance d2) => (_ = d1.AllInch() != d2.AllInch()? true : false);
+        
+        public override string ToString() => $"{foot}'-{inch}\"";
+
+    }
+    
+}
---
+++ Lab12/testWork/DistanceInch/Program.cs
@@ -0,0 +1,11 @@
+﻿using DistanceInch;
+
+Distance a = new Distance(20, 11);
+Distance b = new Distance(20, 8);
+Distance c = a+b;
+Distance d = a-b;
+
+Console.WriteLine(c.ToString());
+Console.WriteLine(d.ToString());
+Console.WriteLine(a != b);
+Console.WriteLine(a == b);
